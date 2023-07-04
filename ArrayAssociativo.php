d3359b317b535ed24528e104eca6a6634822f107
Rafael Lopes Soares <rafael.lopes.soares@accenture.com>
2023-07-04 15:15:20 -0300

    Organiznado!

--- ArrayAssociativo.php
+++
@@ -1,8 +0,0 @@
-<?php
-$filme = [
-    'titulo' => 'Pulp fiction',
-    'ano' => 1992,
-    'genero' => 'Drama'
-];
-print_r($filme);
-?>
--- ArrayMultidimensional.php
+++
@@ -1,9 +0,0 @@
-<?php
-$filme = [
-    'titulo' => 'Pulp fiction',
-    'ano' => 1992,
-    'genero' => 'Drama',
-    'Cast' => ['John Travolta', 'Bruce Willis', 'Uma Thurman', 'Samuel L Jackson'],
-];
-print_r($filme);
-?>
